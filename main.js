2bd266270555c11b67a3d7e3db85ec97aa016494
Laura Parra <laupafer93@gmail.com>
2024-07-04 10:49:26 +0200

    change some clases

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ CONTABILIZAR LOS INTENTOS/CLICKS DE LA USUARIA*/
 
 const button = document.querySelector (".js-button");
 const inputNumber = document.querySelector (".js-inputNumber");
-const track = document.querySelector (".js-p-track");
+const clue = document.querySelector (".js-p-clue");
 const tries = document.querySelector (".tries");
 
 function getRandomNumber(max) { 
@@ -29,13 +29,13 @@ function comparateValues() {
     const inputValue = parseInt(inputNumber.value);
 
      if (inputValue < 1 || inputValue > 100){
-         track.innerHTML = "El número debe estar entre 1 y 100"
+         clue.innerHTML = "El número debe estar entre 1 y 100"
      } else if (inputValue > getNumber) {
-        track.innerHTML = "El número que has introducido es demasiado alto" 
+        clue.innerHTML = "El número que has introducido es demasiado alto" 
     } else if (inputValue < getNumber) {
-        track.innerHTML = "El número que has introducido es demasiado bajo"
+        clue.innerHTML = "El número que has introducido es demasiado bajo"
     }else if (inputValue === getNumber){
-        track.innerHTML = "¡HAS GANADO CAMPEONA!"
+        clue.innerHTML = "¡HAS GANADO CAMPEONA!"
     }
 }
 
