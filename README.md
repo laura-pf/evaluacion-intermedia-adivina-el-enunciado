5747bdac57bbd4d4cef24bfec5b9e40c963499c7
Laura Parra <laupafer93@gmail.com>
2024-08-27 11:06:03 +0200

    fixed excersice

---
+++ README.md
@@ -0,0 +1,52 @@
+## JUEGO ADIVINA EL NUMERO
+
+## Índice
+
+- [Descripción](#descripción)
+- [Instalación](#instalación)
+- [Uso](#uso)
+- [Características](#características)
+- [Contacto](#contacto)
+
+## Descripción
+
+Este proyecto consta de un juego en el que se genera un numero aleatorio del 1 al 100 cada vez que se recarga la página, donde la persona tiene que introducir valores en el input para tratar de adivinarlo. Dependiendo del numero introducido, devuelve pistas para acertar el numero.
+
+## Instalación
+
+Las instrucciones para instalar el proyecto:
+
+git clone https://github.com/laura-pf/evaluacion-intermedia-modulo-3.git
+
+## Uso
+
+Para iniciar este proyecto:
+
+ir al index.html y abrir con Live Server
+
+## Características
+
+Este proyecto incluye las siguientes características claves:
+
+1.  **Generador de número aleatorio**
+
+Cada vez que se recarga la pagina, se genera un numero aleatorio que no es visible para la persona que juega, solo se puede ver a través de la consola.
+
+2.  **Input**
+
+Input para introducir el número que se trata de adivinar.
+
+3.  **Boton de prueba**
+    Para ver si el numero introducido coincide con el numero aleatorio generado, cada vez que se le da al botón, se van sumando los intentos generados al contador de numero de intentos
+
+4.  **Mensajes de pista e intentos**
+    Son mensajes que se actualizan cuando el usuario hace click en prueba:
+
+- el primero compara el umero escrito en el input con el generado y cambia según el resultado de dicha comparación.
+- el segundo, ya mencionado en el apartao 3. son los intentos que hace la persona cada vez que el usuario le da click al botón de prueba.
+
+## Contacto
+
+Para cualquier consulta, sugerencia o reporte de problemas, puedes ponerte en contacto con:
+
+- [Laura Parra](https://github.com/laura-pf)
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /*Cuando la usuaria hace click en el botón:
 se recogen los datos del input que ha escrito la usuaria
@@ -13,40 +13,42 @@ EVENTO CLICK DONDE PASAN DOS COSAS:
 SE COMPARA ESE NUMERO CON EL QUE HA ESCRITO LA USUARIA CON EL NUMERO ALEATORIO GENERADO
 CONTABILIZAR LOS INTENTOS/CLICKS DE LA USUARIA*/
 
-const button = document.querySelector (".js-button");
-const inputNumber = document.querySelector (".js-inputNumber");
-const clue = document.querySelector (".js-p-clue");
-const tries = document.querySelector (".tries");
+const button = document.querySelector(".js-button");
+const inputNumber = document.querySelector(".js-inputNumber");
+const clue = document.querySelector(".js-p-clue");
+const tries = document.querySelector(".tries");
 
-function getRandomNumber(max) { 
-  return Math.ceil(Math.random() * max); 
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
 }
 
 const getNumber = getRandomNumber(100);
 console.log("el numero generado es:", getNumber);
 
 function comparateValues() {
-    const inputValue = parseInt(inputNumber.value);
+  const inputValue = parseInt(inputNumber.value);
 
-     if (inputValue < 1 || inputValue > 100){
-         clue.innerHTML = "El número debe estar entre 1 y 100"
-     } else if (inputValue > getNumber) {
-        clue.innerHTML = "El número que has introducido es demasiado alto" 
-    } else if (inputValue < getNumber) {
-        clue.innerHTML = "El número que has introducido es demasiado bajo"
-    }else if (inputValue === getNumber){
-        clue.innerHTML = "¡HAS GANADO CAMPEONA!"
-    }
+  if (!inputValue) {
+    clue.innerHTML = "Por favor, introduce un numero";
+  } else if (inputValue < 1 || inputValue > 100) {
+    clue.innerHTML = "Pista: El número debe estar entre 1 y 100";
+  } else if (inputValue > getNumber) {
+    clue.innerHTML = "Pista: El número que has introducido es demasiado alto";
+  } else if (inputValue < getNumber) {
+    clue.innerHTML = "Pista: El número que has introducido es demasiado bajo";
+  } else if (inputValue === getNumber) {
+    clue.innerHTML = "¡HAS GANADO CAMPEONA!";
+  }
 }
 
 let clickCount = 0;
-const handleClick = (event) => {
-    event.preventDefault();
-    comparateValues();
-    
 
-    clickCount++;
-    tries.innerHTML = `Número de intentos: ${clickCount}`
-}
+const handleClick = (event) => {
+  event.preventDefault();
+  comparateValues();
+
+  clickCount++;
+  tries.innerHTML = `Número de intentos: ${clickCount}`;
+};
 
 button.addEventListener("click", handleClick);
